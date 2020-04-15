4b7c3d7d39b37cc8541c268b8c59853794292255
IvarsKarpics <ivars.karpics@gmail.com>
2020-04-15 09:50:58 +0200

    Test codacy

--- ispyb/examples/client.py
+++ ispyb/examples/client.py
@@ -1,13 +1,13 @@
 import requests
 
-root_url = 'http://localhost:8000/ispyb/api/v1' 
-paths = ['/prop']
+root_url = "http://localhost:8000/ispyb/api/v1"
+paths = ["/prop"]
 for path in paths:
-    print('-----------------------------------------')
-    print('Request: %s%s' %(root_url, path))
+    print("-----------------------------------------")
+    print("Request: %s%s" % (root_url, path))
     r = requests.get(root_url + path)
-    print('Status code: %d' % r.status_code)
+    print("Status code: %d" % r.status_code)
     data = r.json()
     for d in data:
         print(d)
-    #print('Data: %s' % str(data))
+    # print('Data: %s' % str(data))
