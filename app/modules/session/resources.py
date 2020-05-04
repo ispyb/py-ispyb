7b470c6df6a7d45f663954f534c0ee9a976a650a
IvarsKarpics <ivars.karpics@gmail.com>
2020-05-04 20:06:03 +0200

    changed routes

--- app/modules/session/resources.py
+++ app/modules/session/resources.py
@@ -5,9 +5,29 @@ from app.modules.session.schemas import f_session_schema, ma_session_schema
 api = Namespace("Session", description="Session related namespace", path="/session")
 
 
-@api.route("")
+def get_all_sessions():
+    session_list = SessionModel.query.all()
+    return ma_session_schema.dump(session_list, many=True)
+
+
+def get_proposal_sessions(proposal_id):
+    session_list = SessionModel.query.filter_by(proposalId=proposal_id)
+    return ma_session_schema.dump(session_list, many=True)
+
+@api.route("/list")
 class SessionList(Resource):
+
     @api.doc(security="apikey")
     def get(self):
-        session_list = SessionModel.query.all()
-        return ma_session_schema.dump(session_list)
+        return get_all_sessions()
+
+
+@api.route("/proposal/<int:proposal_id>")
+@api.param("proposal_id", "Proposal id (integer)")
+@api.doc(description="proposal_id should be an integer ")
+#@token_required
+class ProposalSessionList(Resource):
+
+    def get(self, proposal_id):
+        """Returns all proposal shippings"""
+        return get_proposal_sessions(proposal_id)
--- app/modules/shipping/resources.py
+++ app/modules/shipping/resources.py
@@ -5,9 +5,28 @@ from app.modules.shipping.schemas import f_shipping_schema, ma_shipping_schema
 api = Namespace("Shipping", description="Shipping related namespace", path="/shipping")
 
 
+def get_all_shippings():
+    shipping_list = ShippingModel.query.all()
+    return ma_shipping_schema.dump(shipping_list, many=True)
+
+def get_proposal_shippings(proposal_id):
+    shipping_list = ShippingModel.query.filter_by(proposalId=proposal_id)
+    return ma_shipping_schema.dump(shipping_list, many=True)
+
 @api.route("")
 class ShippingList(Resource):
     @api.doc(security="apikey")
     def get(self):
-        shipping_list = ShippingModel.query.all()
-        return ma_shipping_schema.dump(shipping_list)
+        return get_all_shippings()
+
+@api.route("/proposal/<int:proposal_id>")
+@api.param("proposal_id", "Proposal id (integer)")
+@api.doc(description="proposal_id should be an integer ")
+#@token_required
+class ProposalShippingList(Resource):
+
+    def get(self, proposal_id):
+        """Returns all proposal shippings"""
+        return get_proposal_shippings(proposal_id)
+
+
