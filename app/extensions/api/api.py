34ca62e966d2aff77a608c9d0ad358c3a8593e97
Ivars Karpics <ivars.karpics@gmail.com>
2020-06-09 21:23:32 +0200

    Added abstract base class for authentication

--- app/extensions/api/api.py
+++ app/extensions/api/api.py
@@ -1,8 +1,27 @@
 # encoding: utf-8
-"""
-Extended Api implementation with an application-specific helpers
-----------------------------------------------------------------
-"""
+#
+#  Project: py-ispyb
+#  https://github.com/ispyb/py-ispyb
+#
+#  This file is part of py-ispyb software.
+#
+#  py-ispyb is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  py-ispyb is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with py-ispyb. If not, see <http://www.gnu.org/licenses/>.
+
+
+__license__ = "LGPLv3+"
+
+
 from six import iteritems
 
 from flask_restx import Api as BaseApi
