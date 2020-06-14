bbe0df3bbee2e39f3d38976a96fde6755fb52868
Ivars Karpics <ivars.karpics@gmail.com>
2020-06-14 21:19:47 +0200

    fixed scripts

--- scripts/generate_models.sh
+++ scripts/generate_models.sh
@@ -1,3 +1,3 @@
 flask-sqlacodegen --flask --outfile ../models.py mysql://mxuser:mxpass@localhost/pydb_test
 
-sed -i -e 's/db = SQLAlchemy()/from app.extensions import db/g' ../models.py
+sed -i -e 's/db = SQLAlchemy()/from app.extensions import db/g' ../app/models.py
