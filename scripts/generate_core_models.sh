224bde5e0127cd21721d5c26fa8d449903b34953
IvarsKarpics <ivars.karpics@gmail.com>
2020-11-25 11:37:37 +0100

    exclude views

--- scripts/generate_core_models.sh
+++ scripts/generate_core_models.sh
@@ -1,6 +1,6 @@
 URI="$(grep SQLALCHEMY_DATABASE_URI $1)"
 URI="$(echo "$URI" | sed -e "s/SQLALCHEMY_DATABASE_URI://" | tr -d '"')"
-flask-sqlacodegen --flask --nobackrefs --outfile ../ispyb_core/models.py $URI
+flask-sqlacodegen --flask --nobackrefs --noviews --outfile ../ispyb_core/models.py $URI
 
 sed -i -e 's/db = SQLAlchemy()/from app.extensions import db/g' ../ispyb_core/models.py
 sed -i -e 's/class AutoProcStatu(db.Model)/class AutoProcStatus(db.Model)/g' ../ispyb_core/models.py
