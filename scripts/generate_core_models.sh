9c3487e1b04d83082df5542b0b4b587d48400a8e
IvarsKarpics <ivars.karpics@gmail.com>
2020-11-25 11:30:25 +0100

    Fix scripts

--- scripts/generate_core_models.sh
+++ scripts/generate_core_models.sh
@@ -1,11 +1,7 @@
-USER="mxuser"
-PWD="mxpass"
-HOST="localhost"
-DB_NAME="pydb_test"
-
-flask-sqlacodegen --flask --nobackrefs --outfile ../ispyb_core/models.py mysql://$USER:$PWD@$HOST/$DB_NAME
+URI="$(grep SQLALCHEMY_DATABASE_URI $1)"
+URI="$(echo "$URI" | sed -e "s/SQLALCHEMY_DATABASE_URI://" | tr -d '"')"
+flask-sqlacodegen --flask --nobackrefs --outfile ../ispyb_core/models.py $URI
 
 sed -i -e 's/db = SQLAlchemy()/from app.extensions import db/g' ../ispyb_core/models.py
 sed -i -e 's/class AutoProcStatu(db.Model)/class AutoProcStatus(db.Model)/g' ../ispyb_core/models.py
 sed -i -e 's/AutoProcStatu.autoProcIntegrationId/AutoProcStatus.autoProcIntegrationId/g' ../ispyb_core/models.py
-
