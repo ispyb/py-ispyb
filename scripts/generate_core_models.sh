8e5b0a9b75d702b531fc8fc18c28fb2ca0e6c088
IvarsKarpics <ivars.karpics@gmail.com>
2020-11-26 09:42:31 +0100

    fix1

--- scripts/generate_core_models.sh
+++ scripts/generate_core_models.sh
@@ -1,5 +1,5 @@
 URI="$(grep SQLALCHEMY_DATABASE_URI $1)"
-URI="$(echo "$URI" | sed -e "s/SQLALCHEMY_DATABASE_URI://" | tr -d '"')"
+URI="$(echo "$URI"  | tr -d '"' | tr -d ' ' | sed -e "s/SQLALCHEMY_DATABASE_URI://")"
 flask-sqlacodegen --flask --nobackrefs --noviews --outfile ../ispyb_core/models.py $URI
 
 sed -i -e 's/db = SQLAlchemy()/from app.extensions import db/g' ../ispyb_core/models.py
