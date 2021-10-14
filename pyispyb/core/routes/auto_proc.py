c684a3fc34072ae5b325c7b5c2a9268d68e799ee
IvarsKarpics <ivars.karpics@gmail.com>
2021-10-14 12:03:30 +0200

    Download autoproc program attachment files

--- pyispyb/core/routes/auto_proc.py
+++ pyispyb/core/routes/auto_proc.py
@@ -19,7 +19,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with py-ispyb. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from flask import request
+import os
+from flask import request, send_file, abort
 from pyispyb.flask_restx_patched import Resource, HTTPStatus
 
 from pyispyb.app.extensions.api import api_v1, Namespace
@@ -177,7 +178,8 @@ class AutoProcProgramAttachments(Resource):
     @authorization_required
     def get(self):
         """Returns all auto_proc_program attachemnt entries"""
-        return auto_proc.get_auto_proc_program_attachments(request)
+        query_params = request.args.to_dict()
+        return auto_proc.get_attachments_by_query(query_params)
 
     @api.expect(auto_proc_program_attachment_schemas.f_schema)
     @api.marshal_with(auto_proc_program_attachment_schemas.f_schema, code=201)
@@ -187,6 +189,44 @@ class AutoProcProgramAttachments(Resource):
         """Adds a new auto proc program"""
         return auto_proc.add_auto_proc_program_attachment(api.payload)
 
+@api.route("/programs/<int:program_id>/attachments", endpoint="attachments_by_program_id")
+@api.param("program_id", "program id (integer)")
+@api.doc(security="apikey")
+@api.response(code=HTTPStatus.NOT_FOUND, description="auto_proc_program not found.")
+class AttachmentsByAutoProcProgramId(Resource):
+
+    """Return auto proc program attachments by auto proc program id"""
+
+    @api.doc(description="program_id should be an integer ")
+    @token_required
+    @authorization_required
+    def get(self, program_id):
+        """Returns a auto_proc by auto_procId"""
+        return auto_proc.get_attachments_by_query({"autoProcProgramId": program_id})
+
+@api.route(
+    "/programs/<int:program_id>/attachments/download",
+    endpoint="download_attachments_by_program_id")
+@api.param("program_id", "program id (integer)")
+@api.doc(security="apikey")
+@api.response(code=HTTPStatus.NOT_FOUND, description="auto_proc_program not found.")
+class DownloadAttachmentsByAutoProcProgramId(Resource):
+
+    @api.doc(description="program_id should be an integer ")
+    @token_required
+    @authorization_required
+    def get(self, program_id):
+        """Downloads zip file with auto proc attachment files"""
+        attachment_file_zip, msg = auto_proc.get_attachment_zip_by_program_id(program_id)
+
+        if attachment_file_zip:
+            return send_file(
+                attachment_file_zip,
+                attachment_filename='auto_proc_attachments_%d.zip' % program_id,
+                as_attachment=True
+            )
+        else:
+            abort(HTTPStatus.NO_CONTENT, msg)
 
 @api.route("/programs/attachments/<int:attachment_id>", endpoint="attachment_by_id")
 @api.param("attachment_id", "attachment id (integer)")
@@ -209,3 +249,34 @@ class AutoProcProgramAttachmentById(Resource):
     def get(self, attachment_id):
         """Returns a auto_proc by attachment_id"""
         return auto_proc.get_auto_proc_program_attachment_by_id(attachment_id)
+
+
+@api.route("/programs/attachments/<int:attachment_id>/download", endpoint="attachment_download_by_id")
+@api.param("attachment_id", "attachment id (integer)")
+@api.doc(security="apikey")
+@api.response(
+    code=HTTPStatus.NOT_FOUND, description="auto_proc_program_attachment not found."
+)
+class AutoProcProgramAttachmenDownloadById(Resource):
+    """Downloads autoproc program attachment file"""
+
+    @api.doc(description="attachment_id should be an integer ")
+    @token_required
+    @authorization_required
+    def get(self, attachment_id):
+        """Downloads autoproc program attachment file by attachment_id"""
+        attach_dict = auto_proc.get_auto_proc_program_attachment_by_id(attachment_id)
+        path = os.path.join(
+            attach_dict["filePath"],
+            attach_dict["fileName"]
+        )
+        if os.path.exists(path):
+            return send_file(
+                path,
+                as_attachment=True
+            )
+        else:
+            abort(
+                HTTPStatus.NOT_FOUND,
+                "Autoproc program attachment %s not found" % path
+            )
