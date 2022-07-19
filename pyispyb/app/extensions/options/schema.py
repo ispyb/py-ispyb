13c135ffed54c209fb6131e0fd329cb7c9c8c65c
Stuart Fisher <stuart.fisher@esrf.fr>
2022-07-19 14:11:03 +0200

    add db options, add example for legacy routes

--- pyispyb/app/extensions/options/schema.py
+++ pyispyb/app/extensions/options/schema.py
@@ -1,6 +1,24 @@
+<<<<<<< HEAD
 from pydantic import BaseModel, Field
 
 
+=======
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class AdminActivity(BaseModel):
+    username: str
+    action: str
+    comments: str
+    dateTime: datetime
+
+    class Config:
+        orm_mode = True
+
+
+>>>>>>> add db options, add example for legacy routes
 class UIOptions(BaseModel):
     """Publicly available UI options"""
 
@@ -16,5 +34,9 @@ class Options(UIOptions):
         False, title="Query Debugging", description="Enable query debugging"
     )
     enable_legacy_routes: bool = Field(
+<<<<<<< HEAD
         True, title="Legacy Routes", description="Enable legacy routes"
+=======
+        False, title="Legacy Routes", description="Enable legacy routes"
+>>>>>>> add db options, add example for legacy routes
     )
--- pyispyb/app/routes/options.py
+++ pyispyb/app/routes/options.py
@@ -1,9 +1,20 @@
 from fastapi import Depends, Request
 
+<<<<<<< HEAD
 from ...dependencies import permission
 from ..base import AuthenticatedAPIRouter
 from ..extensions.options import base as crud
 from ..extensions.options.schema import Options, UIOptions
+=======
+from ispyb import models
+
+from ...dependencies import pagination, permission
+from ..extensions.database.utils import Paged
+from ...core.schemas.utils import paginated
+from ..base import AuthenticatedAPIRouter
+from ..extensions.options import base as crud
+from ..extensions.options.schema import AdminActivity, Options, UIOptions
+>>>>>>> add db options, add example for legacy routes
 
 router = AuthenticatedAPIRouter(prefix="/options", tags=["Options"])
 
@@ -21,7 +32,11 @@ def get_ui_options() -> UIOptions:
     "",
     response_model=Options,
 )
+<<<<<<< HEAD
 def get_options(depends: bool = Depends(permission("manage_options"))) -> Options:
+=======
+def get_options(depends: bool = Depends(permission("admin_options"))) -> Options:
+>>>>>>> add db options, add example for legacy routes
     """Get the available database options"""
     return crud.get_options(get_all=True)
 
@@ -31,6 +46,7 @@ def get_options(depends: bool = Depends(permission("manage_options"))) -> Option
     response_model=Options,
 )
 def update_options(
+<<<<<<< HEAD
     options: Options, request: Request, depends=Depends(permission("manage_options"))
 ) -> Options:
     """Update the database options"""
@@ -38,3 +54,24 @@ def update_options(
     options = crud.get_options(get_all=True)
     request.app.db_options = options
     return options
+=======
+    options: Options, request: Request, depends=Depends(permission("admin_options"))
+) -> Options:
+    """Update the database options"""
+    crud.update_options(options)
+    options = get_options(get_all=True)
+    request.app.db_options = options
+    return options
+
+
+@router.get(
+    "/activity",
+    response_model=paginated(AdminActivity),
+)
+def get_activity(
+    page: dict[str, int] = Depends(pagination),
+    depends=Depends(permission("admin_options")),
+) -> Paged[models.AdminActivity]:
+    """Get list of admin activity"""
+    return crud.get_activity(**page)
+>>>>>>> add db options, add example for legacy routes
