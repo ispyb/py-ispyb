7593bc6be69ed08a33cc0dbb0d28c39a22329890
Clemente Borges <clemente.borges@desy.de>
2022-09-07 12:55:25 +0200

    Document sync_proposal responses

--- pyispyb/core/routes/webservices/userportalsync.py
+++ pyispyb/core/routes/webservices/userportalsync.py
@@ -1,9 +1,10 @@
 import logging
-from fastapi import HTTPException, status, Depends
-from fastapi.responses import Response, JSONResponse
+from fastapi import HTTPException, Depends
+from fastapi.responses import Response
 from ...modules import userportalsync as crud
 from ...schemas import userportalsync as schema
 from ....dependencies import permission
+from ..responses import Message
 from .base import router
 
 
@@ -12,7 +13,8 @@ logger = logging.getLogger("ispyb")
 
 @router.post(
     "/userportalsync/sync_proposal",
-    response_model=schema.UserPortalProposalSync,
+    response_model=Message,
+    responses={400: {"description": "The input data is not following the schema"}},
 )
 def sync_proposal(
     proposal: schema.UserPortalProposalSync,
@@ -22,21 +24,18 @@ def sync_proposal(
     try:
         execution_time = crud.sync_proposal(proposal=proposal)
         proposal_dict = proposal.dict()
-        return JSONResponse(
-            status_code=200,
-            content={
-                "message": f"The proposal {proposal_dict['proposal']['proposalCode']}"
-                f"-{proposal_dict['proposal']['proposalNumber']} has been synchronized in {execution_time}"
-            },
-        )
+        return {
+            "message": f"The proposal {proposal_dict['proposal']['proposalCode']}"
+            f"-{proposal_dict['proposal']['proposalNumber']} has been synchronized in {execution_time}"
+        }
+
     except Exception as e:
         logging.debug(e)
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.get(
     "/userportalsync/sync_proposal/schema",
-    status_code=status.HTTP_200_OK,
 )
 def get_user_portal_sync_schema() -> str:
     """
