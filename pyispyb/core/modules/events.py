a14b4a4d6ceaaff6721c4f9fdd593db94ddf1835
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-19 08:55:22 +0200

    style

--- pyispyb/core/modules/events.py
+++ pyispyb/core/modules/events.py
@@ -193,7 +193,9 @@ def get_events(
 
         # Apply permissions
         if beamlineGroups:
-            queries[key] = with_beamline_groups(queries[key], beamlineGroups, joinBLSession=False)
+            queries[key] = with_beamline_groups(
+                queries[key], beamlineGroups, joinBLSession=False
+            )
 
         # Filter by session
         if session:
