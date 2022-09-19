e927e70a9d8cc31bb12e1253ab39ea5823b7ae87
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-19 10:27:27 +0200

    document some dc columns

--- pyispyb/core/schemas/events.py
+++ pyispyb/core/schemas/events.py
@@ -1,15 +1,11 @@
 # import datetime
 
-from typing import Union, Optional
+import enum
 from datetime import datetime
+from typing import Union, Optional
 
 from pydantic import BaseModel, Field
 from ispyb import models
-
-d = models.DataCollection
-g = models.DataCollectionGroup
-x = models.XFEFluorescenceSpectrum
-r = models.RobotAction
 
 
 class DataCollectionGroup(BaseModel):
@@ -24,16 +20,54 @@ class DataCollectionMetaData(BaseModel):
     snapshots: dict[str, bool] = Field(description="Snapshot statuses with ids 1-4")
 
 
+class RotationAxis(str, enum.Enum):
+    omega = "omega"
+    phi = "phi"
+
+
 class DataCollection(BaseModel):
-    runStatus: Optional[str]
-    wavelength: Optional[float]
-    exposureTime: Optional[float]
-    numberOfImages: Optional[int]
-    imageDirectory: Optional[str]
-    fileTemplate: Optional[str]
-    imageContainerSubPath: Optional[str]
-    beamSizeAtSampleX: Optional[float]
-    beamSizeAtSampleY: Optional[float]
+    runStatus: Optional[str] = Field(
+        title="Status", description="`Successful` on success"
+    )
+
+    imageDirectory: Optional[str] = Field(
+        title="Directory", description="Directory where the data is saved"
+    )
+    fileTemplate: Optional[str] = Field(
+        title="Data File Template", description="File template for data"
+    )
+    imageContainerSubPath: Optional[str] = Field(
+        title="Image Sub Path", description="For hdf5 files, path to the images"
+    )
+    numberOfImages: Optional[int] = Field(title="Number of Images / Points")
+
+    wavelength: Optional[float] = Field(title="Wavelength", unit="Å")
+    exposureTime: Optional[float] = Field(title="Exposure Time", unit="s")
+    flux: Optional[float] = Field(title="Flux", unit="ph/s")
+    xBeam: Optional[float] = Field(title="Beam Position (Horizontal)", unit="pixels")
+    yBeam: Optional[float] = Field(title="Beam Position (Vertical)", unit="pixels")
+    beamSizeAtSampleX: Optional[float] = Field(
+        title="Beam Size at Sample (Horizontal)", unit="mm"
+    )
+    beamSizeAtSampleY: Optional[float] = Field(
+        title="Beam Size at Sample (Vertical)", unit="mm"
+    )
+    transmission: Optional[float] = Field(title="Beam Transmision", unit="%")
+    resolution: Optional[float] = Field(
+        title="Resolution", description="At edge of detector", unit="Å"
+    )
+    detectorDistance: Optional[float] = Field(title="Detector Distance", unit="mm")
+
+    axisStart: Optional[float] = Field(title="Rotation Axis Start", unit="°")
+    axisEnd: Optional[float] = Field(title="Rotation Axis End", unit="°")
+    axisRange: Optional[float] = Field(title="Rotation Axis Oscillation", unit="°")
+    rotationAxis: Optional[RotationAxis] = Field(title="Exposure Time")
+    overlap: Optional[float] = Field(title="Rotation Axis Overlap", unit="°")
+
+    phiStart: Optional[float] = Field(title="Phi Start", unit="°")
+    kappaStart: Optional[float] = Field(title="Kappa Start", unit="°")
+    omegaStart: Optional[float] = Field(title="Omega Start", unit="°")
+    chiStart: Optional[float] = Field(title="Chi Start", unit="°")
 
     DataCollectionGroup: DataCollectionGroup
 
@@ -55,12 +89,12 @@ class RobotAction(BaseModel):
 class EventBase(BaseModel):
     id: int
     type: str
-    startTime: datetime
-    endTime: Optional[datetime]
+    startTime: Optional[datetime] = Field(title="Start Time")
+    endTime: Optional[datetime] = Field(title="End Time")
     count: int
     session: str
-    blSample: Optional[str] = Field(description="Sample name")
-    blSampleId: Optional[int] = Field(description="Sample id")
+    blSample: Optional[str] = Field(description="Sample Name")
+    blSampleId: Optional[int] = Field(description="Sample Id")
 
     Item: Union[DataCollection, RobotAction]
 
@@ -68,4 +102,3 @@ class EventBase(BaseModel):
 class Event(EventBase):
     class Config:
         orm_mode = True
-        # extra = 'forbid'
